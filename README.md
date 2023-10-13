a9d1db69c52533d69dfd0c2d886a7b66c43fd314
mythridium <132613374+mythridium@users.noreply.github.com>
2023-10-13 14:15:58 +1100

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# [Myth] Incy Wincy Ancient Relics
+
+This mod adds a summary view which has incy wincy ancient relics to easily see all your ancient relics from a glance, with unlocked ancient relics being highlighted.
+
+![Summary](images/summary.png)
+
+A new button has been added on the ancient relics modal allowing you to switch between the summary or the individual skills view.
+
+![Button](images/new-button.png)
+
+This button state is saved so when you reload if you are on the summary view it will remember this and load the summary view.
+
+If you are not viewing the summary, it will load the existing view as is.
+
+Tooltips show locked and unlocked ancient relics.
+
+![Locked](images/locked-tooltip.png)
+
+![Unlocked](images/unlocked-tooltip.png)
---
+++ src/app/main.ts
@@ -0,0 +1,148 @@
+import './main.scss';
+
+export class App {
+    private readonly master = 'assets/media/main/gamemode_ancient_relic.png';
+    private readonly saveKey = 'is-summary';
+
+    private isSummary = false;
+    private tooltips: TippyTooltip[] = [];
+
+    constructor(private readonly context: Modding.ModContext) {}
+
+    public async init() {
+        this.context.onInterfaceReady(() => {
+            if (!game.currentGamemode.allowAncientRelicDrops) {
+                return;
+            }
+
+            this.isSummary = this.context.characterStorage.getItem(this.saveKey) ?? false;
+
+            const modal = document.querySelector('#modal-ancient-relics .block-content');
+
+            if (!modal) {
+                return;
+            }
+
+            const header = document.querySelector('#modal-ancient-relics .block-header .dropdown');
+
+            if (header) {
+                const switchButton = createElement('button', {
+                    className: 'btn btn-secondary',
+                    attributes: [['type', 'button']],
+                    text: this.isSummary ? 'Hide Summary' : 'Show Summary'
+                });
+
+                switchButton.onclick = () => {
+                    this.isSummary = !this.isSummary;
+                    this.context.characterStorage.setItem(this.saveKey, this.isSummary);
+                    switchButton.textContent = this.isSummary ? 'Hide Summary' : 'Show Summary';
+
+                    this.render(modal);
+                };
+
+                header.prepend(switchButton);
+            }
+
+            this.context.patch(<any>Skill, 'onAncientRelicUnlock').after(() => {
+                this.render(modal);
+            });
+
+            this.render(modal);
+        });
+    }
+
+    private render(modal: Element) {
+        const existing = document.getElementById('incy-wincy-ancient-relics');
+
+        if (existing) {
+            existing.remove();
+        }
+
+        for (const tooltip of this.tooltips) {
+            tooltip.destroy();
+        }
+
+        const container = createElement('div', { id: 'incy-wincy-ancient-relics' });
+
+        for (const skill of game.skills.allObjects) {
+            const row = createElement('div', { id: skill.id, className: 'myth-relic-row' });
+
+            for (const ancientRelic of skill.ancientRelics) {
+                const relic = this.createRelic(skill, ancientRelic.relic);
+                row.append(relic);
+            }
+
+            const masterRelic = this.createRelic(skill, skill.completedAncientRelic);
+            row.append(masterRelic);
+
+            container.append(row);
+        }
+
+        modal.append(container);
+
+        if (this.isSummary) {
+            this.show(container);
+            this.hide(modal.querySelector('.row'));
+            this.hide(document.querySelector('#modal-ancient-relics .block-header .dropdown .dropdown-toggle'));
+        } else {
+            this.hide(container);
+            this.show(modal.querySelector('.row'));
+            this.show(document.querySelector('#modal-ancient-relics .block-header .dropdown .dropdown-toggle'));
+        }
+    }
+
+    private show(element: Element | undefined) {
+        element?.classList.remove('d-none');
+    }
+
+    private hide(element: Element | undefined) {
+        element?.classList.add('d-none');
+    }
+
+    private createRelic(skill: AnySkill, ancientRelic: AncientRelic) {
+        const relic = createElement('div', {
+            id: ancientRelic.id,
+            className: `myth-relic${this.isUnlocked(skill, ancientRelic) ? ' unlocked' : ' locked'}`
+        });
+
+        relic.append(
+            createElement('img', {
+                attributes: [
+                    ['src', this.isMasterRelic(skill, ancientRelic) ? this.master : skill.media],
+                    ['height', '24'],
+                    ['width', '24']
+                ]
+            })
+        );
+
+        tippy(relic, {
+            allowHTML: true,
+            delay: 0,
+            duration: 0,
+            content: `
+<div class="myth-ancient-relic-tooltip">
+    <div>${ancientRelic.name}</div>
+    <div class="mt-1">${
+        this.isUnlocked(skill, ancientRelic) || this.isMasterRelic(skill, ancientRelic)
+            ? describeModifierData(ancientRelic.modifiers)
+            : '???'
+    }</div>
+</div>
+        `
+        });
+
+        return relic;
+    }
+
+    private isUnlocked(skill: AnySkill, relic: AncientRelic) {
+        if (this.isMasterRelic(skill, relic)) {
+            return (<any>skill).hasMasterRelic;
+        }
+        const count = skill.getAncientRelicCount(relic);
+        return count >= 1;
+    }
+
+    private isMasterRelic(skill: AnySkill, relic: AncientRelic) {
+        return skill.completedAncientRelic.id === relic.id;
+    }
+}
